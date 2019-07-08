da408b1aad477d9463d7c11c5ca6ca1bfd3e512f
davidagud <davidagud@gmail.com>
2019-07-08 11:07:17 -0500

    Reorganized css files, fixed expansion panel spacing and animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,22 +10,20 @@ import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { LoginComponent } from './auth/login/login.component';
 import { QuestionsModule } from './questions/questions.module';
-import { SignupComponent } from './auth/signup/signup.component';
 
 import { AuthGuard } from './auth/auth.guard';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent,
-    LoginComponent,
     HeaderComponent,
     HomePageComponent
   ],
   imports: [
     AppRoutingModule,
+    AuthModule,
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { MaterialModule } from '../material.module';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     FormsModule,
-    CommonModule
+    CommonModule,
+    MaterialModule
   ]
 })
 export class AuthModule {}
